457eb5da88e3c195666b018d2eb8efd0069b5df6
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-07 23:33:57 +0530

    Refactor Navbar component and add responsive menu icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,61 +2,77 @@ import React from "react";
 import Toggle from "../Toggle/Toggle";
 import "./Navbar.css";
 import { Link } from "react-scroll";
-const navbar = () => {
-	return (
-		<div className="n-wrapper" id="Navbar">
-			{/* left */}
-			<div className="n-left">
-				<div className="n-name">Souhardya Bose</div>
-				<Toggle />
-			</div>
-			{/* right */}
-			<div className="n-right">
-				<div className="n-list">
-					<ul style={{ listStyleType: "none" }}>
-						<li>
-							<Link
-								activeClass="active"
-								to="Navbar"
-								spy={true}
-								smooth={true}
-							>
-								Home
-							</Link>
-						</li>
-						<li>
-							<Link to="About" spy={true} smooth={true}>
-								About
-							</Link>
-						</li>
-						<li>
-							<Link to="services" spy={true} smooth={true}>
-								Serivces
-							</Link>
-						</li>
-						<li>
-							<Link to="gallery" spy={true} smooth={true}>
-								Gallery
-							</Link>
-						</li>
-						<li>
-							<Link to="portfolio" spy={true} smooth={true}>
-								Protfolio
-							</Link>
-						</li>
-						<li>
-							<Link to="testimonial" spy={true} smooth={true}>
-								Testimonial
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<Link to="contact" spy={true} smooth={true}>
-					<button className="button n-button">Contact</button>
-				</Link>
-			</div>
-		</div>
-	);
-};
+import { CiMenuFries } from "react-icons/ci";
+import { useMenuContext } from "../../context/contextAPI";
 
-export default navbar;
+export default function Navbar() {
+	const { isOpen, setIsOpen } = useMenuContext();
+	return (
+		<>
+			<div className="n-wrapper hidden md:flex" id="Navbar">
+				{/* left */}
+				<div className="n-left">
+					<div className="n-name">Souhardya Bose</div>
+					<Toggle />
+				</div>
+				{/* right */}
+				<div className="n-right">
+					<div className="n-list">
+						<ul style={{ listStyleType: "none" }}>
+							<li>
+								<Link
+									activeClass="active"
+									to="Navbar"
+									spy={true}
+									smooth={true}
+								>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link to="About" spy={true} smooth={true}>
+									About
+								</Link>
+							</li>
+							<li>
+								<Link to="services" spy={true} smooth={true}>
+									Serivces
+								</Link>
+							</li>
+							<li>
+								<Link to="gallery" spy={true} smooth={true}>
+									Gallery
+								</Link>
+							</li>
+							<li>
+								<Link to="portfolio" spy={true} smooth={true}>
+									Protfolio
+								</Link>
+							</li>
+							<li>
+								<Link to="testimonial" spy={true} smooth={true}>
+									Testimonial
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<Link to="contact" spy={true} smooth={true}>
+						<button className="button n-button">Contact</button>
+					</Link>
+				</div>
+			</div>
+			<div className="md:hidden flex justify-between items-center mt-4">
+				<div className="n-left">
+					<div className="n-name">Souhardya Bose</div>
+					<Toggle />
+				</div>
+
+				<CiMenuFries
+					size={30}
+					className="cursor-pointer hover:scale-110 transition-all active:scale-90"
+					onClick={() => setIsOpen(!isOpen)}
+				/>
+			</div>
+		</>
+	);
+}
