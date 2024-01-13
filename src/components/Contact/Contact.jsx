9f658c7e119a8baecdfe146e6759794a26e13298
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-14 02:26:22 +0530

    Refactor contact form and update email service

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,64 +1,75 @@
 import React, { useContext, useRef, useState } from "react";
 import "./Contact.css";
-import emailjs from "@emailjs/browser";
 import { themeContext } from "../../Context";
 const Contact = () => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
-  const form = useRef();
-  const [done, setDone] = useState(false)
-  const sendEmail = (e) => {
-    e.preventDefault();
+	const theme = useContext(themeContext);
+	const darkMode = theme.state.darkMode;
+	const [done, setDone] = useState(false);
 
-    emailjs
-      .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
-        form.current,
-        "VLwg1ltOWvnCYAiK_"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-          form.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const messageRef = useRef();
+	const handleSubmit = (e) => {
+		fetch("https://formsubmit.co/your@email.com", {
+			Method: "POST",
+			Headers: {
+				Accept: "application.json",
+				"Content-Type": "application/json",
+			},
+			Body: body,
+			Cache: "default",
+		});
+	};
 
-  return (
-    <div className="contact-form" id="contact">
-      {/* left side copy and paste from work section */}
-      <div className="w-left">
-        <div className="awesome">
-          {/* darkMode */}
-          <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
-          <span>Contact me</span>
-          <div
-            className="blur s-blur1"
-            style={{ background: "#ABF1FF94" }}
-          ></div>
-        </div>
-      </div>
-      {/* right side form */}
-      <div className="c-right">
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
-          <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
-          <input type="submit" value="Send" className="button"/>
-          <span>{done && "Thanks for Contacting me"}</span>
-          <div
-            className="blur c-blur1"
-            style={{ background: "var(--purple)" }}
-          ></div>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className="contact-form" id="contact">
+			{/* left side copy and paste from work section */}
+			<div className="w-left">
+				<div className="awesome">
+					{/* darkMode */}
+					<span style={{ color: darkMode ? "white" : "" }}>
+						Get in Touch
+					</span>
+					<span>Contact me</span>
+					<div
+						className="blur s-blur1"
+						style={{ background: "#ABF1FF94" }}
+					></div>
+				</div>
+			</div>
+			{/* right side form */}
+			<div className="c-right">
+				<form
+					action="https://formsubmit.co/souhardya.bose@ieee.org"
+					method="POST"
+				>
+					<input
+						type="text"
+						name="user_name"
+						className="user"
+						placeholder="Name"
+					/>
+					<input
+						type="email"
+						name="user_email"
+						className="user"
+						placeholder="Email"
+					/>
+					<textarea
+						name="message"
+						className="user"
+						placeholder="Message"
+					/>
+					<input type="submit" value="Send" className="button" />
+					<span>{done && "Thanks for Contacting me"}</span>
+					<div
+						className="blur c-blur1"
+						style={{ background: "var(--purple)" }}
+					></div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
