a132fa7d3043379f57ee76d8687fec3ceef07d15
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-13 16:53:51 +0530

    Add styles for font outline and background gradient in About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,50 @@
 import "./About.css";
 import profile_pic from "../../img/profile-pic.png";
 import laptop_png from "../../img/laptop_png.jpg";
+import { motion } from "framer-motion";
 
 export default function About() {
 	return (
-		<section id="About">
-			<h2 className="text-2xl md:text-4xl font-bold text-center mt-24 mb-4">
+		<section
+			id="About"
+			className="mt-8 flex flex-col justify-between overflow-hidden"
+		>
+			<motion.h2
+				initial={{ opacity: 0, y: "-100" }}
+				whileInView={{ opacity: 1, y: 0 }}
+				transition={{ delay: 0.3 }}
+				className="text-2xl md:text-4xl font-bold text-center mb-4  mt-4 drop-shadow-lg uppercase text-black font-outline-2 font-mono"
+			>
 				A Journey Fueled by Community and Technology
-			</h2>
-			<div className="About flex flex-col md:flex-row items-center">
+				{/* About Me */}
+			</motion.h2>
+			<div className="About flex flex-col md:flex-row items-center mb-10 gap-5">
 				<div
 					className="blur"
 					style={{ background: "rgb(238 210 255)" }}
 				></div>
-				<div className="left flex-1">
-					<img src={profile_pic} alt="" />
+				<div className="left flex-1 ml-10 basis-1/3">
+					<motion.img
+						src={profile_pic}
+						alt=""
+						initial={{ scale: 0 }}
+						whileInView={{ scale: 1 }}
+						transition={{ delay: 0.3 }}
+					/>
 				</div>
-				<div className="right flex-1">
-					<p>
+				<div className="right flex-1 basis-2/3 text-white">
+					<p className="tracking-wide">
 						My story starts with a simple belief:{" "}
-						<strong>people are stronger together </strong>. From a
-						young age, I witnessed the power of communities to
-						support, inspire, and empower individuals. This belief
-						has shaped my life's journey, leading me down a path of
-						community building, technology, and organizational
-						development.
+						<span className="font-bold italic">
+							people are stronger together{" "}
+						</span>
+						. From a young age, I witnessed the power of communities
+						to support, inspire, and empower individuals. This
+						belief has shaped my life's journey, leading me down a
+						path of community building, technology, and
+						organizational development.
 					</p>
+					<br />
 					<p>
 						My early experiences in event management and technical
 						product specialization instilled in me a deep
@@ -34,6 +53,7 @@ export default function About() {
 						these events further fueled my passion for fostering
 						vibrant communities.
 					</p>
+					<br />
 					<p>
 						My journey has been one of continuous learning and
 						growth. I am passionate about the power of communities
